2e58719a2f6176151d6268f5f13508b236cdad64
Allen West <58156836+allenwest24@users.noreply.github.com>
2022-07-12 06:03:23 -0400

    Create solution.rs

---
+++ Vowel Count/solution.rs
@@ -0,0 +1,9 @@
+fn get_count(string: &str) -> usize {
+    let mut vowels_count: usize = 0;
+    for c in string.chars() {
+        if c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u' {
+            vowels_count += 1;
+        }
+    }
+    vowels_count
+}
